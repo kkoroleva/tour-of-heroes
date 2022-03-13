709327800289e2115c25f35fadcb16dd2ee95222
kkoroleva <korolevakarinka22@gmail.com>
2022-03-13 16:39:18 +0300

    first step

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent implements OnInit {
+
+  constructor() { }
+
+  hero : Hero = {
+    id: 1,
+    name: 'Miss Raven',
+  };
+  ngOnInit(): void {
+  }
+
+}
