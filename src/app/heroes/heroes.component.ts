b52cb373558c1aef67b8afca30523d693ad72cf1
kkoroleva <korolevakarinka22@gmail.com>
2022-03-14 21:27:46 +0300

    basic tutorial

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -7,14 +9,31 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
+  heroes: Hero[] = [];
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
-  hero : Hero = {
-    id: 1,
-    name: 'Miss Raven',
-  };
   ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero.id).subscribe();
   }
 
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Mr G' },
+  { id: 2, name: 'Godrick Griffindor' },
+  { id: 3, name: 'Flash' },
+  { id: 4, name: 'Batman' },
+  { id: 5, name: 'Winnie-the-Pooh' },
+  { id: 6, name: 'Robin' },
+  { id: 7, name: 'Cat Woman' },
+  { id: 8, name: 'Gindjer' },
+  { id: 9, name: 'Sonik' },
+  { id: 10, name: 'Artorias' }
+];
