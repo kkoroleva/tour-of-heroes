83362cf0f5355edf5785a1c20bdfb7d559f18770
kkoroleva <korolevakarinka22@gmail.com>
2022-03-19 16:08:06 +0300

    add expandable rows

--- src/app/book/book-table/book-table.component.ts
+++ src/app/book/book-table/book-table.component.ts
@@ -1,17 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { Book } from "../book";
 import { BooksData } from '../book-mock';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-book-table',
   templateUrl: './book-table.component.html',
-  styleUrls: ['./book-table.component.scss']
+  styleUrls: ['./book-table.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class BookTableComponent implements OnInit {
 
   booksData: Book[] = [];
   bookColumns: string[] = [];
   booksDataLength: number = 0;
+  expandedElement: Book | null = null;
 
   constructor() {
 
@@ -19,11 +28,15 @@ export class BookTableComponent implements OnInit {
 
   ngOnInit(): void {
     this.booksData = BooksData;
-    this.bookColumns = ['id', 'title', 'sold'];
+    this.bookColumns = ['id', 'title', 'releasedBooks'];
     this.booksDataLength = this.booksData.length;
-
   }
-  getSoldNumber() {
+
+  getColumnNameStraight(str: string):string {
+    return str === 'id'? 'ID' : str === 'title'? 'Название' : str === 'releasedBooks'? 'Продано (шт.)' : '';
+  }
+
+  getSoldNumber(): number {
     return this.booksData.map(book => book.releasedBooks).reduce((acc, value) => acc + value, 0);
   }
 }
